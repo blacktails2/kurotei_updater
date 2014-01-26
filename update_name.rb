dddbe9ec9d59c9cf5812723f4e0587d70c39cc94
yukkuri_sinai <yukkuri.pro@gmail.com>
2014-01-26 16:52:34 +0900

    NGワードを実装

--- update_name.rb
+++ update_name.rb
@@ -1,5 +1,6 @@
 # Coding: UTF-8
 require 'twitter'
+require 'yaml'
 require './key.rb'
 
 CONSUMER_KEY    = Conf::CONSUMER_KEY
@@ -24,7 +25,13 @@ end
 @orig_name, @screen_name = [:name, :screen_name].map{|x| @rest_client.user.send(x) }
 
 def ng_word?(name)
-    return false
+    ng_words = YAML.load_file("ng_word.yml")
+    if ng_words != false
+        ng_words.each do |ng_word|
+            return true if name.include?(ng_word)
+        end
+    end
+    false
 end
 
 def update_name(status)
