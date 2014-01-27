6361889b5683787e7957b6e758b67e36e506e909
blacktails2 <blacktails2@gmail.com>
2014-01-27 23:20:14 +0900

    数字だったらどのような数字を入れても返すように変更

--- up_kazoo04.rb
+++ up_kazoo04.rb
@@ -25,21 +25,18 @@ end
 
 
 def up_kazoo04(status)
-    picture = ["It's dynamic kazoo04. http://t.co/jBGzCuKzhZ", "It's beautiful kazoo04. http://t.co/NlL7jRdqjC", "It's moonlight kazoo04. http://t.co/rw7AqkT3kz", "It's old kazoo04. http://t.co/RkCwaPKsSn", "It's transpicuous kazoo04. http://t.co/WH522CHrtT", "It's google kazoo04. http://t.co/Ok7D4JB3tQ", "It's google kazoo04. http://t.co/PA5FN97dp5", "It's cool kazoo04. http://t.co/JAp0RLIFbB", "It's writing kazoo04. http://t.co/MpE9sozJ5j", "It's lightning kazoo04 & grapswiz. http://t.co/TcdUigS3C0"]
+    pictures = ["It's dynamic kazoo04. http://t.co/jBGzCuKzhZ", "It's beautiful kazoo04. http://t.co/NlL7jRdqjC", "It's moonlight kazoo04. http://t.co/rw7AqkT3kz", "It's old kazoo04. http://t.co/RkCwaPKsSn", "It's transpicuous kazoo04. http://t.co/WH522CHrtT", "It's google kazoo04. http://t.co/Ok7D4JB3tQ", "It's google kazoo04. http://t.co/PA5FN97dp5", "It's cool kazoo04. http://t.co/JAp0RLIFbB", "It's writing kazoo04. http://t.co/MpE9sozJ5j", "It's lightning kazoo04 & grapswiz. http://t.co/TcdUigS3C0"]
     begin
-        if status.text.match(/^@#{@screen_name}[\s　]*up_kazdesign[\s　]*(.+)/) #@sn update_name名前がマッチしてるか調べる
+        if status.text.match(/^@#{@screen_name}[\s　]*up_kazdesign[\s　]*(\d+)/) #@sn update_name名前がマッチしてるか調べる
             number = $1 #抽出
             else #それでもない場合
             return #戻す
         end
-        
-        rescue => e #例外をeと定義
+    rescue => e #例外をeと定義
         p status, status.text
         p e #例外をターミナルに書き出す
-        else 
-        
-        text = @orig_name == number ? "元に戻したよ！" : "picture[number]" #元の名前の場合は元に戻した、指定された場合はi have just...
-        @rest_client.update("@#{status.user.screen_name} #{text}") #textで定義された物を呟く
+    else 
+        @rest_client.update("@#{status.user.screen_name} #{pictures[number.to_i % pictures.length]}", :in_reply_to_status_id => status.id)
     end
 end
 
