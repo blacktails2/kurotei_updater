5ccd9c906c0f079f1d6b1362ee05dd1b7eccd0ea
blacktails2 <blacktails2@gmail.com>
2014-02-14 15:33:48 +0900

    かずー氏をふぁぼっていく

---
+++ kaztweetfavs.rb
@@ -0,0 +1,50 @@
+# Coding: UTF-8
+require 'twitter'
+require 'yaml'
+require './key.rb'
+
+CONSUMER_KEY    = Conf::CONSUMER_KEY
+CONSUMER_SECRET = Conf::CONSUMER_SECRET
+ACCESS_TOKEN    = Conf::ACCESS_TOKEN
+ACCESS_SECRET   = Conf::ACCESS_SECRET
+
+@rest_client = Twitter::REST::Client.new do |config|
+    config.consumer_key        = CONSUMER_KEY
+    config.consumer_secret     = CONSUMER_SECRET
+    config.access_token        = ACCESS_TOKEN
+    config.access_token_secret = ACCESS_SECRET
+end
+
+@stream_client = Twitter::Streaming::Client.new do |config|
+    config.consumer_key       = CONSUMER_KEY
+    config.consumer_secret    = CONSUMER_SECRET
+    config.oauth_token        = ACCESS_TOKEN
+    config.oauth_token_secret = ACCESS_SECRET
+end
+
+@orig_name, @screen_name = [:name, :screen_name].map{|x| @rest_client.user.send(x) }
+
+
+def kaztweetfavs(status)
+    begin
+        tweet = Twitter.user_timeline("kazoo04")
+        if /^.*@#{@screen_name}[\s　]*.*/ =~ tweet
+        elsif /^.*くろてい.*/ =~ tweet
+            Twitter.favorite(id, options={tweet})
+        else #それでもない場合
+            return #戻す
+        end
+        
+    rescue => e #例外をeと定義
+        p e #例外をターミナルに書き出す
+    else
+    end
+end
+
+@stream_client.user do |object|
+    next unless object.is_a? Twitter::Tweet
+    
+    unless object.text.start_with? "RT"
+        update_name(object)
+    end
+end
